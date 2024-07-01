f0a51b20326a3b66964b6fd3d0f4ffaa0d12b619
samihafidiDLW <84071784+samihafidiDLW@users.noreply.github.com>
2024-07-01 15:26:05 +0200

    Update DancingGoatConstants.cs

--- examples/DancingGoat/DancingGoatConstants.cs
+++ examples/DancingGoat/DancingGoatConstants.cs
@@ -7,6 +7,7 @@
         /// The constraint limits the match to a list of specified controllers for pages not handled by the content tree-based router.
         /// The constraint ensures that broken URLs lead to a "404 page not found" page and are not handled by a controller dedicated to the component or
         /// to a page handled by the content tree-based router (which would lead to an exception).
+        ///
         /// </summary>
         public const string CONSTRAINT_FOR_NON_ROUTER_PAGE_CONTROLLERS = "Account|Consent";
 
